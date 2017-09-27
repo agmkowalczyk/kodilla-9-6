76ae49412ccf8d4fa65d7c73b9adeb2e2b40d9d8
agmkowalczyk <agm.kowalczyk@gmail.com>
2017-09-27 14:28:25 +0200

    Kodilla 9-2, tablice

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
+//  ----- ZADANIE 9.2 -----
+
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotr', 'Marek', 'Arek', 'Jarek'];
+var allNames = femaleNames.concat(maleNames);
+var newName = "Michał";
+
+if (allNames.indexOf(newName) === -1) {
+  allNames.push(newName);
+  console.log( 'Imię "' + newName + '" zostało dodane.');
+} else {
+  console.log( 'Imię "' + newName + '" jest już na liście.');
+}
+
+
+
+
+/*
 //  ----- ZADANIE 9.1 -----
 
 var triangleArea;
@@ -17,3 +35,5 @@ var triangle3Area = getTriangleArea( -3, 7 );
 
 
 //https://github.com/jamesallardice/jslint-error-explanations/blob/master/message-articles/return-conditional.md
+
+*/
